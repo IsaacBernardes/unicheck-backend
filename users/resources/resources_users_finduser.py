2c1d110dd5526da80aabd91b6d4efb0af39786ab
IsaacBernardes <isaacjoasb@gmail.com>
2022-09-25 15:52:30 -0300

    Endpoint para filtrar usuários

---
+++ users/resources/resources_users_finduser.py
@@ -0,0 +1,62 @@
+import psycopg2
+import sanic.response
+
+from utils.keycloack import KeycloackClient
+
+__creator__ = "IsaacBernardes"
+__last_modifier__ = "IsaacBernardes"
+__last_modify__ = "25/09/2022"
+__version__ = open("version").read()
+
+api_results = {
+    "success": {"status": 200, "message": "Operação realizada com sucesso"},
+    "keyError": {"status": 400, "message": "Os dados fornecidos são insuficientes ou inválidos"},
+    "dataError": {"status": 400, "message": "Erro ao realizar a operação no banco de dados"},
+    "databaseError": {"status": 400, "message": "Erro no banco de dados"},
+    "invalidToken": {"status": 401, "message": "O token do usuário não foi reconhecido"},
+    "invalidUser": {"status": 401, "message": "O usuário informado não foi cadastrado"},
+    "notAuthorized": {"status": 401, "message": "O usuário não possui acesso a esta funcionalidade"},
+    "defaultError": {"status": 500, "message": "Erro interno da API"}
+}
+
+
+def finduser_resolver(request, context=None):
+
+    keycloack_client = KeycloackClient()
+    keycloack_client.connect()
+    situation = "defaultError"
+    data = []
+
+    try:
+
+        user_email = request["params"]["email"]
+        search_path = "/admin/realms/$REALM/users?email" + user_email
+        result = keycloack_client.get(search_path)
+
+        if result[0] == 200:
+            situation = "success"
+            data = result[1]
+        else:
+            situation = "invalidUser"
+
+    except KeyError as ex:
+        print("Key error: " + str(ex))
+        situation = "keyError"
+    except (psycopg2.IntegrityError, psycopg2.DataError, psycopg2.NotSupportedError) as ex:
+        print("Data error: " + str(ex))
+        situation = "dataError"
+    except (psycopg2.DatabaseError, psycopg2.Error) as ex:
+        print("Database error: " + str(ex))
+        situation = "databaseError"
+    except Exception as ex:
+        print("Default error: " + str(ex))
+        situation = "defaultError"
+
+    finally:
+        keycloack_client.disconnect()
+        return sanic.response.json(body={
+            "message": api_results[situation]["message"],
+            "data": data,
+            "version": __version__,
+        }, status=api_results[situation]["status"])
+
--- users/server.py
+++ users/server.py
@@ -2,7 +2,7 @@ import sanic
 from sanic import Sanic
 from sanic_openapi import openapi, openapi3_blueprint
 
-from resources.resources_users_listusers import listusers_resolver
+from resources.resources_users_finduser import finduser_resolver
 from resources.resources_users_signin import signin_resolver
 from resources.resources_users_signup import signup_resolver
 from resources.resources_users_validatetoken import validatetoken_resolver
@@ -22,24 +22,50 @@ app = Sanic("UserApp")
 app.blueprint(openapi3_blueprint)
 
 
-@app.get("/api/users")
-@openapi.summary("List Users")
-@openapi.description("Route destined to list users according user's permissions")
-@openapi.parameter("Authentication", str, location="header", required=True)
-@openapi.parameter("name", str, location="query", required=False)
-@openapi.parameter("profile", int, location="query", required=False)
-@openapi.response(200, {"application/json": ListUsersRequestSuccessResponse}, "Operation completed successfully")
-@openapi.response(400, {"application/json": ListUsersRequestErrorResponse}, "There was a problem with the data provided")
-@openapi.response(401, {"application/json": ListUsersRequestErrorResponse}, "User is not authenticated or does not have access to this functionality")
-@openapi.response(500, {"application/json": ListUsersRequestErrorResponse}, "Internal server error")
-async def list_users(request: sanic.Request):
+# @app.get("/api/users")
+# @openapi.summary("List Users")
+# @openapi.description("Route destined to list users according user's permissions")
+# @openapi.parameter("Authentication", str, location="header", required=True)
+# @openapi.parameter("name", str, location="query", required=False)
+# @openapi.parameter("profile", int, location="query", required=False)
+# @openapi.response(200, {"application/json": ListUsersRequestSuccessResponse}, "Operation completed successfully")
+# @openapi.response(400, {"application/json": ListUsersRequestErrorResponse}, "There was a problem with the data provided")
+# @openapi.response(401, {"application/json": ListUsersRequestErrorResponse}, "User is not authenticated or does not have access to this functionality")
+# @openapi.response(500, {"application/json": ListUsersRequestErrorResponse}, "Internal server error")
+# async def list_users(request: sanic.Request):
+#     try:
+#         req = {
+#             "headers": request.headers,
+#             "body": {},
+#             "params": request.query_args
+#         }
+#         return listusers_resolver(req)
+#     except Exception as ex:
+#         print(ex)
+#         return sanic.response.json(body={
+#             "message": "Erro ao receber a requisição",
+#             "version": __version__,
+#             "data": []
+#         }, status=500)
+
+
+@app.get("/api/users/find")
+@openapi.summary("Find Users by E-mail")
+@openapi.description("Route destined to list users by e-mail")
+@openapi.parameter("Authorization", str, location="header", required=True)
+@openapi.parameter("email", str, location="query", required=True)
+# @openapi.response(200, {"application/json": ListUsersRequestSuccessResponse}, "Operation completed successfully")
+# @openapi.response(400, {"application/json": ListUsersRequestErrorResponse}, "There was a problem with the data provided")
+# @openapi.response(401, {"application/json": ListUsersRequestErrorResponse}, "User is not authenticated or does not have access to this functionality")
+# @openapi.response(500, {"application/json": ListUsersRequestErrorResponse}, "Internal server error")
+async def find_user(request: sanic.Request):
     try:
         req = {
             "headers": request.headers,
             "body": {},
             "params": request.query_args
         }
-        return listusers_resolver(req)
+        return finduser_resolver(req)
     except Exception as ex:
         print(ex)
         return sanic.response.json(body={
@@ -52,7 +78,7 @@ async def list_users(request: sanic.Request):
 @app.put("/api/users")
 @openapi.summary("Insert User")
 @openapi.description("Route destined to add a new user to the system")
-@openapi.parameter("Authentication", str, location="header", required=True)
+@openapi.parameter("Authorization", str, location="header", required=True)
 @openapi.body({"application/json": InsertUserRequestBody}, required=True)
 @openapi.response(200, {"application/json": InsertUserRequestSuccessResponse}, "Operation completed successfully")
 @openapi.response(400, {"application/json": InsertUserRequestErrorResponse}, "There was a problem with the data provided")
@@ -106,6 +132,7 @@ async def insert_user(request: sanic.Request):
 @app.post("/api/users/validatetoken")
 @openapi.summary("Validate token")
 @openapi.description("Verify if user token is valid")
+@openapi.parameter("Authorization", str, location="header", required=True)
 async def validate_token(request: sanic.Request):
 
     try:
